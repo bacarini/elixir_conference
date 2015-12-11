5ea49a64550ab460a5adaf7035a426ea9f17ed37
Bruno Bacarini <bacarini.bruno@gmail.com>
2015-12-11 19:33:58 -0200

    finding way to resolve problem

--- lib/elixir_conference.ex
+++ lib/elixir_conference.ex
@@ -1,14 +1,14 @@
 defmodule ElixirConference do
-  def main(file_name) do
+  def main(file_name, number_of_tracks \\ 2) do
    File.open(file_name)
-   |> process_file
+   |> process_file(number_of_tracks)
   end
 
-  defp process_file({:ok, file}) do
+  defp process_file({:ok, file}, number_of_tracks) do
     file
     |> Parser.parse
-    |> Organizer.execute
+    |> Organizer.execute(number_of_tracks)
   end
 
-  defp process_file({:error, reason}), do: IO.puts "ERROR: #{:file.format_error(reason)}"
+  defp process_file({:error, reason}, _), do: IO.puts "ERROR: #{:file.format_error(reason)}"
 end
--- lib/organizer.ex
+++ lib/organizer.ex
@@ -1,13 +1,39 @@
 require IEx
+require Logger
 
 defmodule Organizer do
   @morning 180
   @afternoon 240
 
-  def execute(talks) do
+  def execute(talks, number_of_tracks) do
     total_tracks = find_total_tracks(talks)
     IO.puts "Total of tracks: #{total_tracks}"
-    Enum.map(talks, &test(&1))
+    IO.puts "Number of tracks: #{number_of_tracks}"
+    for _ <- 1..number_of_tracks do
+      find_tracks(@morning, talks)
+    end
+  end
+
+  defp find_tracks(time, talks) do
+   # Logger.info "talks: #{talks}"
+    minutes = 0
+    for %{talk: talk, duration: duration} <- talks do
+IO.puts "min: #{minutes}"
+      tmp = minutes + duration
+      if tmp > time, do: :exit
+      minutes = minutes + duration
+
+      IO.puts "talk: #{talk} - Duration: #{duration} - Time: #{time}"
+    end
+  end
+
+  defp find_session(%{talk: talk, duration: duration}, time) do
+    IO.puts "talk: #{talk} - Duration: #{duration} - Time: #{time}"
+    minutes = 0
+    tmp = minutes + duration
+    return if tmp > time
+
+    { talk, minutes } = { duration, tmp }
   end
 
   defp test(%{duration: duration, talk: talk}) do
